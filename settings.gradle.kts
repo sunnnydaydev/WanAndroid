89192b2c271db5c7eb60c6c214147306984e21de
SunnyDay <1246771571@qq.com>
2024-09-18 21:55:36 +0800

    add ui layer

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "WanAndroid"
 include(":app")
- 
+include(":uilayer")
--- uilayer/src/main/java/com/carry/wanandroid/uilayer/ui/activities/MainActivity.kt
+++ uilayer/src/main/java/com/carry/wanandroid/uilayer/ui/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.carry.wanandroid
+package com.carry.wanandroid.uilayer.ui.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.carry.wanandroid.uilayer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
