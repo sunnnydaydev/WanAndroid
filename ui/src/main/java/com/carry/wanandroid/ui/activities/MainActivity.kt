148c3bfcbabdb8f43effc72ac8d7b8183a17057a
SunnyDay <1246771571@qq.com>
2024-09-21 17:42:33 +0800

    add navigation in project

--- ui/src/main/java/com/carry/wanandroid/ui/activities/MainActivity.kt
+++ ui/src/main/java/com/carry/wanandroid/ui/activities/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.carry.wanandroid.ui.R
+import com.carry.wanandroid.ui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        NavigationUI.setupWithNavController(binding.bottomNav,getNavController())
     }
+    private fun getNavController() = (supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment).navController
 }
